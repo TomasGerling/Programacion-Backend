11fd3da3f6f94e601be60cf3c9384b4ab7134683
TomasGerling <tomasgerling@hotmail.com>
2022-10-17 20:11:46 -0300

    Se realizo lo pedido para realizar el desafio numero 2

--- actividad.js
+++ actividad.js
@@ -1,85 +1,101 @@
 const fs = require('fs')
 
-class Contenedor{
-    constructor(name){
-        this.name = name
+class Contenedor {
+    constructor(productName) {
+    this.productName = productName
     }
+    save(producto) {
+    const productos = fs.readFileSync(this.productName,'utf-8')
+    const parsedProductos = JSON.parse(productos)
 
-    save(producto){
-        const productos = fs.readFileSync(`${this.name}.txt`, 'utf-8')
-        const parsedProductos = JSON.parse(productos);
-        let idMax = 0
-        if(parsedProductos.length == 0){
-            Object.assign(producto, {
-                id: 1
-            })
-            parsedProductos.push(producto)
-            fs.writeFileSync(`${this.name}.txt`, JSON.stringify(parsedProductos, null, 2))
-            return 1
-        }else{
-            parsedProductos.forEach(i => {
-                if(i.id > idMax){
-                    idMax = i.id
-                }
-            })
-            Object.assign(producto, {
-                id: idMax + 1
-            })
-            parsedProductos.push(producto)
-            fs.writeFileSync(`${this.name}.txt`, JSON.stringify(parsedProductos, null, 2))
-            return (idMax + 1)
+    let idMaximo = 0
+
+    if (parsedProductos.length == 0) {
+        Object.assign(producto, {
+            id: 1
+        })
+        parsedProductos.push(producto)
+        fs.writeFileSync(this.productName, JSON.stringify(parsedProductos, null, 2))
+        return 1
+    } else {
+        parsedProductos.forEach(i => {
+        if (i.id > idMaximo) {
+            idMaximo = i.id
         }
-
+    })
+    Object.assign(producto, {
+        id: idMaximo + 1
+    })
+    parsedProductos.push(producto)
+        fs.writeFileSync(this.productName, JSON.stringify(parsedProductos, null, 2))
+        return (idMaximo + 1)
     }
-
-    async deleteById(id){
-        try{
-            const datos = await fs.promises.readFile(this.nombreArchivo,"utf-8")
-
-            const arrayDatos = await JSON.parse(datos);
-            
-            const resultadoFiltro = arrayDatos.filter((item) => item.id !== id);
-
-            fs.promises.writeFile(this.nombreArchivo, JSON.stringify(resultadoFiltro))
-        } catch (error){
-            throw new Error(error)
+    }
+    async getByID(id) {
+    try {
+        const data = await fs.promises.readFile(this.productName, "utf-8")
+        const jsonData = JSON.parse(data)
+        const elementofiltrado = jsonData.find(item => item.id == id)
+        if (elementofiltrado == undefined) {
+        return null
+        } else {
+            return elementofiltrado
+        }
+        } catch (error) {
+        throw new Error(error)
+    }
+}
+    async getAll() {
+        try {
+        const data = await fs.promises.readFile(this.productName, "utf-8")
+        const jsonData = JSON.parse(data)
+        return jsonData
+        } catch (error) {
+        throw new Error(error)
         }
     }
-    deleteAll(){
-        try{
-            fs.writeFileSync(`${this.name}.txt`, "")
-        } catch (error){
-            throw new Error(error)
+    async deleteById(id) {
+    try {
+        const data = await fs.promises.readFile(this.productName, "utf-8")
+        const jsonData = JSON.parse(data)
+        const elementoFiltrado = jsonData.filter(item => item.id == id)
+        if (elementoFiltrado == undefined) {
+        console.error("no hay nada")
+        } else {
+        const newArray = jsonData.filter(item => item.id != id)
+        for (let i = 0; i < newArray.length; i++) {
+            newArray[i] = { ...newArray[i], id: (i + 1) }
+        }
+        await fs.promises.writeFile(this.productName, JSON.stringify(newArray, null, 2))
+
+    }
+    } catch (error) {
+        throw new Error(error)
+    }
+}
+
+    async deleteAll() {
+        try {
+            const arrayVacio = []
+            await fs.promises.writeFile(this.productName, JSON.stringify(arrayVacio, null, 2))
+
+        } catch (error) {
+            throw new Error
         }
     }
 }
-const products = [{
-    ID:1,
-    name:"Mesa redonda",
-    price:200
-},
-{
-    ID:2,
-    name: "Mesa triangular",
-    price:200
-},
-{
-    ID:3,
-    name:"Mesa cuadrada",
-    price:200
-}]
-let newProduct= [{
-    name: "Mesa rectangular",
-    price: 200
-}]
-const container = new Contenedor('productos')
 
-// console.log(container.save({
-//     name: "Libro2",
-//     price: 22.32
-// }))
+const container = new Contenedor('productos.txt')
 
-// console.log(container.save({
-//     name: "Libro54",
-//     price: 21.32
-// }))
+console.log(container.save({
+    title: "Mesa Rectangular",
+    price: 400,
+}))
+console.log(container.save({
+    title: "Mesa de escritorio",
+    price: 540,
+}))
+console.log(container.save({
+    title: "Mesa ratona",
+    price: 1600,
+}));
