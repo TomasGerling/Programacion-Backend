4b42d3c882488a23564aeeb92f5edf9c78ca7146
TomasGerling <tomasgerling@hotmail.com>
2022-11-17 19:18:45 -0300

    Se realizo la primera pre entrega

--- src/app.js
+++ src/app.js
@@ -1,30 +1,19 @@
-import dotenv from "dotenv";
+import "dotenv/config";
 import express from "express";
-import ejsConfig from "./config/ejs.js";
 import errorHandler from "./middlewares/errorHandler.js";
 import apiRouter from "./routes/api.js";
 import http from "http";
-import { CustomSocket } from "./config/socketio.js";
-import clientRouter from "./routes/client.js";
-import messagesService from "./services/messages.service.js";
-import { chatEvents } from "./controllers/chat.socket.js";
+import morgan from "morgan";
 
-dotenv.config();
-
-// Express and server socket config
 const app = express();
 const server = http.createServer(app);
-export const socketInstance = new CustomSocket(server);
 
-// Templates configure
-ejsConfig(app);
-
+app.use(morgan("combined"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(process.cwd + "/public"));
 
 // Client views and API routes
-app.use(clientRouter);
 app.use("/api", apiRouter);
 
 // API health check
